3c1cef1970e49c6aaf855af2f9d5a28583e8aa80
Paul Schou <schou@umbc.edu>
2012-02-07 06:32:58 -0500

    update mkdirs to only modify perms on new directories created

--- aslutil/mkdirs.m
+++ aslutil/mkdirs.m
@@ -9,15 +9,20 @@ function mkdirs(dir,perms,groups)
 %   mkdirs('/tmp/mod/me','+w +x','o g');
 
 % Written by Paul Schou - 1 July 2009  (paulschou.com)
-if ~exist(dir,'dir')
-  if(nargin > 1)
+if ~exist(dir)
+
+  % check to see if the parent exists by calling this function recursively
+  if(nargin > 1)  % if we should pass the attributes to the parent tests
     mkdirs(fileparts(dir),perms,groups);
   else
     mkdirs(fileparts(dir));
   end
-  mkdir(dir);
-end
 
-if(nargin > 1)
-  fileattrib(dir,perms,groups);
+  % make the needed directory
+  mkdir(dir);
+
+  % if we should set the attributes do so here
+  if(nargin > 1)
+    fileattrib(dir,perms,groups);
+  end
 end
