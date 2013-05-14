9a7402eea1b61140dd3fd97991232b92c4b6193b
Breno Imbiriba <breno.imbiriba@gmail.com>
2013-05-14 15:33:46 -0400

    create_rcalc using version number

--- cris/rtp/create_rcalc_ecm_subset.m
+++ cris/rtp/create_rcalc_ecm_subset.m
@@ -1,8 +1,17 @@
 cris_paths
 
-model = 'ecm';
-emis = 'wis';
-input_glob = [prod_dir '/' datestr(JOB(1),'yyyy/mm/dd') '/cris_sdr*sub*.' datestr(JOB(1),'yyyy.mm.dd') '*.rtp'];
+sdate = JOB(1);
+edate = JOB(2);
 
-yyyymmdd = JOB(1);
-sarta_core(input_glob, yyyymmdd, model, emis);
+model = 'ecm';
+emis = 'dan'; %emis = 'wis';
+
+version = 'Rv1.1b-Mv1.1b';
+
+rtpset='subset';
+src='noaa_ops';
+
+
+input_glob = [prod_dir '/' datestr(sdate,'yyyy/mm/dd') '/cris_sdr60_' rtpset '_' src '.' datestr(sdate,'yyyy.mm.dd') '.*.' version '.rtp'];
+
+sarta_core(input_glob, sdate, model, emis);
