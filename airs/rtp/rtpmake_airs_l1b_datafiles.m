3bb8a0afda90860d184f79719d4526fbccaac4c6
Breno Imbiriba <breno.imbiriba@gmail.com>
2013-07-08 12:01:41 -0400

    Adding AIRS rtpmake code

---
+++ airs/rtp/rtpmake_airs_l1b_datafiles.m
@@ -0,0 +1,106 @@
+function [head hattr prof pattr] = rtpmake_airs_l1b_datafiles(files)
+% function [head hattr profi pattr] = rtpmake_airs_l1b_datafiles(files)
+% 
+% files - list of l1b AIRS files
+%
+% Breno Imbiriba - 2013.06.25
+% Based on excerpts of Paul's rtp_core.m 
+
+
+  nodata = -9999;
+
+
+  for ifile = 1:length(files)  % loop over granule files for a day
+
+    disp(['Loading file ' files{ifile}]);
+
+
+    %%%%%%
+    % 
+    % Read AIRS Data File
+    %
+    %%%%%%
+
+    [eq_x_tai, f, gdata]=readl1b_all(files{ifile});
+
+
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % 
+    % Construct PROFILE structure
+    % 
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    data.rtime = gdata.rtime(:)';  %'
+    nok = length(data.rtime);
+
+    data.rlat = gdata.rlat(:)';  %'
+    data.rlon = gdata.rlon(:)';  %'
+    data.satazi = gdata.satazi(:)'; %'
+    data.satzen = gdata.satzen(:)'; %'
+    data.zobs = gdata.zobs(:)'; %'
+    data.solazi = gdata.solazi(:)'; %'
+    data.solzen = gdata.solzen(:)'; %'
+    data.robs1 = gdata.robs1;
+
+    data.atrack = gdata.atrack(:)'; %'
+    data.xtrack = gdata.xtrack(:)'; %'
+
+    data.calflag = gdata.calflag;
+
+    % write out the file index number for which file the data came from
+    %   note: file names & dates are only available in the summary file
+    data.findex = gdata.findex(:)'; %'
+
+
+
+    %%%%
+    %
+    % Fill out the iudef values
+    %
+    %%%%
+    
+    data.iudef = nodata*ones(10,nok);
+
+
+    %%
+    % fill out the iudef values
+    %% 
+
+    data.udef = nodata*ones(20,nok);
+    data.udef(1,:) = eq_x_tai(:)' - gdata.rtime(:)';
+
+
+    prof(ifile) = data;
+
+  end   % loop over files
+
+  prof = structmerge(prof);
+
+  nchan = size(prof.robs1,1);
+  part1 = (1:nchan)';
+
+  % Determine channel freqs
+  vchan = f(:);
+
+  %%%%%%%%%  Make a HEADER structure
+  head = struct;
+  head.pfields = 4;
+  head.ptype = 0;
+  head.ngas = 0;
+  
+  % Assign RTP attribute strings
+  hattr={ {'header' 'pltfid' 'Aqua'}, ...
+	  {'header' 'instid' 'AIRS'} };
+  %
+  pattr={ {'profiles' 'rtime' 'seconds since 1 Jan 1993'}, ...
+	  {'profiles' 'robsqual' '0=good, 1=bad'}, ...
+	  {'profiles' 'udef(1,:)' 'eq_x_tai - rtime'} };
+
+  % PART 1
+  head.instid = 800; % AIRS 
+  head.nchan = length(part1);
+  head.ichan = part1;
+  head.vchan = vchan(part1);
+  %set_attr(hattr,'rtpfile',[rtp_outfile]);
+
+end
--- cris/rtp/create_rcalc_ecm_allfov_with_flist.m
+++ cris/rtp/create_rcalc_ecm_allfov_with_flist.m
@@ -1,7 +1,7 @@
 cris_paths
 
 model = 'ecm';
-emis = 'wis';
+emis = 'dan';
 
 % JOB comes from a list of rtp_1 files
 
@@ -11,11 +11,17 @@ input_glob = JOB;
 
 % /asl/data/rtprod_cris/yyyy/mm/dd/cris_sdr60_noaa_ops.2012.08.10.123.v1.rtp
 
+%          1         2         3         4         5         6         7         8         9     
+% 123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+% /asl/data/rtprod_cris/2013/04/01/cris_sdr60_noaa_ops.2013.04.01.135.Rv1.1b-Mv1.1b.rtp
 thisdate = datenum(str2num(JOB([54:57])), str2num(JOB([59:60])), str2num(JOB([62:63])) ,str2num(JOB([65:67]))/144*24,0,0);
+
 JOB=thisdate
 
 say(['File: ' input_glob ]);
 say(['This date: ' datestr(thisdate)]);
 
+yyyymmdd = datestr(thisdate,'yyyymmdd');
 
-sarta_core
+sarta_core(input_glob, yyyymmdd, model, emis);
+
