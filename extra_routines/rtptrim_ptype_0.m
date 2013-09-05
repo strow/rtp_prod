0136b31e1ee77be4388425eeabfe45383f287366
Breno Imbiriba <breno.imbiriba@gmail.com>
2013-09-05 11:55:28 -0400

    Process files contain level profiles - add a routine to simplify the process (rtptrim_ptype_0)

---
+++ extra_routines/rtptrim_ptype_0.m
@@ -0,0 +1,37 @@
+function [head hattr prof pattr] = rtptrim_ptype_0(head, hattr, prof, pattr, parent)
+% function [head hattr prof pattr] = rtptrim_ptype_0(head, hattr, prof, pattr, parent)
+%
+% Sometimes we want to trim a calculated RTP file but keeping only the "level"
+% profiles, which will remain saved in the parent file.
+%
+% This routine trims the provided *calculated* RTP structures, removing level 
+% gases and adjusting the header appropriatedely.
+%
+% INPUT
+%   head, hattr, prof, pattr - RTP structures after klayers and sarta. 
+%   parent - parent file name.
+% 
+% OUTPUT
+%   head, hattr, prof, pattr - Trimmed RTP structures.
+%    
+% Breno Imbiriba - 2013.09.05
+
+
+  % Remove ptype=1 type profiles:
+
+  prof = rmfield(prof,{'gas_1','gas_2','gas_3','gas_4','gas_5','gas_6',...
+                       'gas_9','gas_12','plevs','palts','ptemp'});
+
+  [head hattr prof pattr] = rtptrim(head,hattr,prof,pattr,'parent',parent);
+
+
+  % Fix header 
+  % - Remove fields modified by Klayers
+  head = rmfield(head, {'glist','gunit','ngas','pmin','pmax'});
+  % - Change ptype to Level profiles
+  head.ptype = 0;
+
+
+
+end
+
