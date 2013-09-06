085ef338e575be4ec73431e39b701569ed2e3cfb
L. Larrabee Strow <strow@umbc.edu>
2013-09-06 11:09:11 -0400

    Cleaned up README.md, but needs more work.  Cosmetic modifications to example_cris_sdr_files.m (removed many empty lines).

--- scripts/example_cris_sdr_files.m
+++ scripts/example_cris_sdr_files.m
@@ -1,15 +1,8 @@
-
-
-
-
-
-
 % Input directory
 idir = '/home/imbiriba/tmp/ftp.ssec.wisc.edu/pub/ssec/davet/';
 
 % Input file names
 fnames={'SCRIS_npp_d20130510_t2029459_e2037437_b07952_c20130511023743801999_noaa_ops.h5', 'SCRIS_npp_d20130515_t2037059_e2045037_b08023_c20130516024502741669_noaa_ops.h5', 'SCRIS_npp_d20130516_t2020579_e2028557_b08037_c20130517022855059234_noaa_ops.h5', 'SCRIS_npp_d20130530_t0939059_e0947037_b08229_c20130530154704782914_noaa_ops.h5', 'SCRIS_npp_d20130531_t0922579_e0930557_b08243_c20130531153057447240_noaa_ops.h5', 'SCRIS_npp_d20130601_t0906499_e0914477_b08257_c20130601151448862865_noaa_ops.h5'};
-
 
 % Loop over file names
 for ifile = 5:numel(fnames)
@@ -27,7 +20,6 @@ for ifile = 5:numel(fnames)
     [head prof] = subset_rtp(head, prof, [], [], find(~lbad_rtime & ~lbad_geo));
   end
 
-
   % 2. Add Model, etc...
 
   % Add Topography 
@@ -35,7 +27,6 @@ for ifile = 5:numel(fnames)
 
   % Add Atm Model
   [head hattr prof pattr] = rtpadd_ecmwf_data(head,hattr,prof,pattr);
-
  
   % Add diurnal Stemp 
   %[head hattr prof pattr] = driver_gentemann_dsst(head,hattr, prof,pattr);
@@ -43,27 +34,21 @@ for ifile = 5:numel(fnames)
   % Add surface Emissivity
   [head hattr prof pattr] = rtpadd_emis_Wisc(head,hattr,prof,pattr);
 
-
   % 3. Run CLEAR flag detection
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  % Do clear selection - for now this is instrument dependent
+
+% Do clear selection - for now this is instrument dependent
   instrument='CRIS'; %'IASI','CRIS'
   [head hattr prof pattr summary] = ...
                    compute_clear_wrapper(head, hattr, prof, pattr, instrument);
 
-
-
   % 4. Run Calcs:
   tempfile = mktemp('temp.rtp');
   KlayersRun(head,hattr,prof,pattr,tempfile,11);
-  [ head hattr prof pattr] = SartaRun(tempfile, 12);
+  [head hattr prof pattr] = SartaRun(tempfile, 12);
    
-
   % 5. Save data
-
   [dd nn ex] = fileparts(fnames{ifile});
   fout = [idir '/' nn '.rtp'];
-
 
   rtpwrite(fout, head,hattr,prof,pattr);
 
