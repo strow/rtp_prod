68b0b4a4792940066267651774ff1e5e70f5eb4e
Breno Imbiriba <breno.imbiriba@gmail.com>
2013-12-04 10:00:04 -0500

    Moving some CrIS file name tools to cris/utils

--- cris/utils/cris_ccast_filenames.m
+++ cris/utils/cris_ccast_filenames.m
@@ -31,6 +31,8 @@ function [file_list nfiles] = cris_ccast_filenames(sdate, edate, asldata, dataty
       path = [asldata '/cris/ccast/sdr60_dtf/' num2str(yyyy,'%04d') '/' num2str(ddd,'%03d') ];
     elseif(strcmpi(datatype, 'ccast_sdr60'))
       path = [asldata '/cris/ccast/sdr60/' num2str(yyyy,'%04d') '/' num2str(ddd,'%03d') ];
+    elseif(strcmpi(datatype, 'ccast_sdr60_hr'))
+      path = [asldata '/cris/ccast/sdr60_hr/' num2str(yyyy,'%04d') '/' num2str(ddd,'%03d') ];
     else
       error(['Unknonw data type ' datatype '.']);
     end
