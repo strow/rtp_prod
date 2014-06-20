0f74af041b7def1a2e9b0af65afb489d1a183b1a
L. Larrabee Strow <strow@umbc.edu>
2014-06-20 13:30:41 -0400

    New test script: rtp_by_file.m, now 1 SDR file --> 1 RTP file

---
+++ scripts/rtp_by_file.m
@@ -0,0 +1,89 @@
+% rtp_by_file.m
+%
+% Script to run much of rtp_prod processing interactively at home by
+% L. Strow.
+% 
+% This script is the first part of testing a large re-write of
+% rtp_prod grib readers, and maybe emissivity assignments.
+%
+% June, 20, 2014
+
+rtprod = '~/Git/rtp_prod';
+matlib = '~/Git/matlib';
+gribtype = 'ecmwf';
+% Combination of matlib and rtp_prod git hash/tags
+version = version_number();
+
+fin_hdr  = fullfile('/asl/data/cris/ccast/sdr60_hr','2013','240');
+fout_hdr = fullfile('/asl/s1/strow/rtprod_cris','2013','08','28');
+
+flist    = dir(fullfile(fin_hdr,'SDR*.mat'));
+
+% Before any loops on filenames, assign current grib name as empty
+current_ename = '';
+
+addpath(rtprod);
+paths
+setenv('RTPROD',rtprod);
+
+% Just do the first file for now
+ifile = 1;
+
+fn_in = fullfile(fin_hdr,flist(ifile).name);
+
+% Vary name here, add version, remove SDR, etc.
+fn_out = flist(ifile).name;
+fn_out = strrep(fn_out,'SDR_','cris_ccast_hr_');
+fn_out = fullfile(fout_hdr,fn_out);
+fn_out_dir = fileparts(fn_out);
+
+disp('Input File:');
+disp(fn_in);
+disp('Output File:');
+disp(fn_out);
+
+if(~exist(fn_out_dir,'dir'))
+  disp(['Creating output directory']);
+  mkdir(fn_out_dir);
+end
+
+% Test for file existence
+if(exist(fn_out ,'file'))
+  disp(['Attention: Output file "' fn_out '" already exist. Skipping...']); 
+  return
+end
+
+% Read SDR file
+[head hattr prof pattr] = sdr2rtp_bc(fn_in); 
+
+% Assign some headers
+head.ngas = 0;
+hattr = set_attr(hattr,'version',version);
+
+% Remove buggy CrIS rtime, rlat, and rlon
+[head prof] = cris_filter_bad_data(head, prof);
+
+% Add landfrac
+[head hattr prof pattr] = rtpadd_usgs_10dem(head,hattr,prof,pattr,'/asl');
+
+% Get grib data
+[head, hattr, prof, pattr, current_ename] = ...
+    rtpadd_grib_data(current_ename, gribtype, head, hattr, prof, pattr);
+
+% Assign emissivities (land and ocean)
+[head hattr prof pattr] = rtpadd_emis_DanZhou(head,hattr,prof,pattr);
+
+% Find clear FOVs and compute SARTA clear
+% instrument='CRIS_888';
+% [head hattr prof pattr summary] = ...
+%        compute_clear_wrapper(head, hattr, prof, pattr, instrument);
+
+% Save output
+%   [dd, ~, ~] = fileparts(output_file_obs1);
+%   if(~exist(dd,'dir'))
+%     system(['mkdir -p ' dd]);
+%   end 
+%   disp(['Saving ' output_file_obs1]);
+%   rtpwrite(output_file_obs1, head,hattr,prof,pattr);
+
+
